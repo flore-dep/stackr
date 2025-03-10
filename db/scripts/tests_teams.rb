366588d069e6c40f8928f7683d6380726af1cc4b
romain decline <83162519+romain-decline@users.noreply.github.com>
2025-03-10 18:09:55 +0100

    same

--- db/scripts/tests_teams.rb
+++ db/scripts/tests_teams.rb
@@ -11,3 +11,5 @@ p Team.new(name: "Tech").tap(&:valid?).errors.full_messages
 p Team.new(organization: Organization.first).tap(&:valid?).errors.full_messages
 p Team.new(name: "aa", organization: Organization.first).tap(&:valid?).errors.full_messages
 p Team.new(name: Team.first.name, organization: Team.first.organization).tap(&:valid?).errors.full_messages
+
+puts "DESTROYS"
