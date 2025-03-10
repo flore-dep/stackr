233d8e31e09d61b7d0290382262a975684430587
romain decline <83162519+romain-decline@users.noreply.github.com>
2025-03-10 18:09:44 +0100

    tests destroys organizations

--- db/scripts/tests_organizations.rb
+++ db/scripts/tests_organizations.rb
@@ -11,3 +11,14 @@ p Organization.new().tap(&:valid?).errors.full_messages
 p Organization.new(name: "").tap(&:valid?).errors.full_messages
 p Organization.new(name: "he").tap(&:valid?).errors.full_messages
 p Organization.new(name: Organization.first.name).tap(&:valid?).errors.full_messages
+
+
+puts "DESTROYS"
+
+organization = Organization.create(name: "some name")
+team = Team.create(name: "Marketing", organization: organization)
+puts "Before : Organizations #{Organization.count} - Teams #{Team.count}"
+organization.destroy
+puts "after : Organizations #{Organization.count} - Teams #{Team.count}"
+puts "Organization exists: #{Organization.exists?(organization.id)}"
+puts "Team exists: #{Team.exists?(team.id)}"
