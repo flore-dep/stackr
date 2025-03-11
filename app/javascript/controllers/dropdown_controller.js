babf2a9a414e5af18375329ada4ab8b381c09c0e
jeannaegelen <jean.naegelen@gmail.com>
2025-03-11 12:17:49 +0100

    sidebar dropdown done

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  connect() {
-    console.log ("hello ?")
+  static targets = ["menu"];
+
+  toggle () {
+    this.menuTarget.classList.toggle("active");
   }
 }
