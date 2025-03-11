e3bbe5f22dd2e3d6119a246b9a795f21c4bff234
Victor Benhamou <vbenhamou98@gmail.com>
2025-03-11 13:15:23 +0100

    pundit for basic get routes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,10 @@
 class TeamsController < ApplicationController
+  def index
+    @teams = policy_scope(Team)
+  end
+
+  def show
+    @team = Team.find(params[:id])
+    authorize @team
+  end
 end
