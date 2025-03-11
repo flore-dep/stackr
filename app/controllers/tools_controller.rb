8e5b86944b5a97d8b10b556488a15d13c4ac5b32
jeannaegelen <jean.naegelen@gmail.com>
2025-03-11 12:56:03 +0100

    commit avant merge des appartenances

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,2 +1,12 @@
 class ToolsController < ApplicationController
+  before_action :set_team
+  def index
+    @tools = team.tools
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
 end
