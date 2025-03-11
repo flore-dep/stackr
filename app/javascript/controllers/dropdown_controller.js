389086c330b68d3df5d50f691fdb51d976e2daa9
jeannaegelen <jean.naegelen@gmail.com>
2025-03-11 11:50:55 +0100

    controller stimulus added

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+  connect() {
+    console.log ("hello ?")
+  }
+}
