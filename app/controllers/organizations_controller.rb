b3ddf19984b0aa9669256a8fa2288f6c4cb41667
jeannaegelen <jean.naegelen@gmail.com>
2025-03-11 16:45:51 +0100

    sidebar done GOOOOOOOOOOOOOOOD

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,13 +1,13 @@
 class OrganizationsController < ApplicationController
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
-  
+
   def index
     @organizations = policy_scope(Organization)
   end
 
   def show
-    @organization = Organization.find(params[:id])
+    @organization = current_user.organization
     authorize @organization
   end
 end
--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,6 +2,6 @@ class ToolsController < ApplicationController
   skip_after_action :verify_authorized
   skip_after_action :verify_policy_scoped
   def index
-    @tools = @team.tools
+    @tools = Tool.all
   end
 end
