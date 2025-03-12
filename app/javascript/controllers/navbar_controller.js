37dddbfc10769329c9c3a203602aaa0f73324092
Victor Benhamou <vbenhamou98@gmail.com>
2025-03-12 16:42:14 +0100

    first commit

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ["members", "tools", "licenses", "membersPart", "toolsPart", "licensesPart"]
+
+  fire(event) {
+    this.hideAllPartials()
+    this.removeActiveClasses()
+    event.currentTarget.classList.add("nvbactive")
+    this.showPartial(event.currentTarget)
+  }
+
+  hideAllPartials() {
+    this.membersPartTarget.classList.add("d-none")
+    this.toolsPartTarget.classList.add("d-none")
+    this.licensesPartTarget.classList.add("d-none")
+  }
+
+  removeActiveClasses() {
+    this.membersTarget.classList.remove("nvbactive")
+    this.toolsTarget.classList.remove("nvbactive")
+    this.licensesTarget.classList.remove("nvbactive")
+  }
+
+  showPartial(button) {
+    if (button === this.membersTarget) {
+      this.membersPartTarget.classList.remove("d-none")
+    } else if (button === this.toolsTarget) {
+      this.toolsPartTarget.classList.remove("d-none")
+    } else if (button === this.licensesTarget) {
+      this.licensesPartTarget.classList.remove("d-none")
+    }
+  }
+}
