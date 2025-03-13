ed911ea9a77c1aed591979f4cfb54ad8a440e97c
jeannaegelen <jean.naegelen@gmail.com>
2025-03-13 15:37:41 +0100

    Adding done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,22 @@ class UsersController < ApplicationController
     @licenses = current_user.licenses
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @team = Team.find(params[:team_id])
+    @user.team = @team
+    if @user.save!
+      redirect_to team_path(@team)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :role, :start_date, :end_date, :password)
+  end
 end
