2dd98146a984e4c516e08752612a457c184e2c37
jeannaegelen <jean.naegelen@gmail.com>
2025-03-17 15:32:51 +0100

    When testing

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,20 @@
 class PlansController < ApplicationController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+
+  def create
+    @plan = Plan.new(plan_params)
+    @tool = Tool.find(params[:tool_id])
+    @plan.organisation = current_user.organization.find(params[:organization_id])
+
+    if @plan.save
+      redirect_to team_path(@team)
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:status, :formula)
+  end
 end
