223f90d9cc991577bcca31e3a3abe6131f04ed7d
jeannaegelen <jean.naegelen@gmail.com>
2025-03-18 11:49:02 +0100

    license creation done

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,5 +1,26 @@
 class LicensesController < ApplicationController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
   def create
-    @license = License.new
+    @team = current_user.team
+    @license = License.new(license_params)
+    @plan = Plan.find(params[:plan_id])
+    @license.plan = @plan
+    @license.user = current_user
+    @license.status = "Pending"
+
+    @tool = @license.plan.tool
+
+    if @license.save!
+      redirect_to team_path(@team)
+    else
+      redirect_to tool_path(@tool)
+    end
+  end
+
+  private
+
+  def license_params
+    params.require(:license).permit(:start_date, :end_date, :status, :access_type)
   end
 end
