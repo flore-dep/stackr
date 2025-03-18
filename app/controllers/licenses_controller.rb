cb1a8397ffb50ea5df506fb1a8c18ee871a34be7
Flore de Paillerets <flore.depaillerets@gmail.com>
2025-03-18 10:21:11 +0100

    decline accept function

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -2,4 +2,14 @@ class LicensesController < ApplicationController
   def create
     @license = License.new
   end
+
+  def accept
+    @license = License.find(params[:id])
+    @license.update(status: "Approved")
+  end
+
+  def reject
+    @license = License.find(params[:id])
+    @license.update(status: "Declined")
+  end
 end
