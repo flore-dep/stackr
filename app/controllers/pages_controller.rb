aafdbc3b54c9e7b3c577fc156c9dff6a448ae447
jeannaegelen <jean.naegelen@gmail.com>
2025-03-20 15:41:35 +0100

    ca marche

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def home
-    costs_per_team_per_month
+    costs_per_month
     active_licenses_per_month
     tools_by_category
   end
@@ -23,45 +23,22 @@ class PagesController < ApplicationController
     Rails.logger.debug "Tools per team per month data: #{@chart_data.inspect}"
   end
 
-  def costs_per_team_per_month
-    start_date = 1.year.ago
-    @cost_data = Team.all.map do |team|
-      monthly_costs = team.tools
-                          .where("tools.created_at >= ? AND tools.created_at <= ?", start_date, Time.now)
-                          .group_by { |tool| tool.created_at.beginning_of_month }
-                          .transform_values { |tools| tools.sum { |tool| tool_cost(tool) } }
-      {
-        name: team.name,
-        data: monthly_costs
-      }
+  def costs_per_month
+    object = []
+    (1..12).each do |m|
+      sub = []
+      date = Time.new(2025, m, 1)
+      sub.push(date)
+      licenses = License.where("start_date <= ? AND end_date > ?", date, date)
+      cost = 0
+      licenses.each do |l|
+        cost += l.plan.formula[1]
+      end
+      sub.push(cost)
+      object.push(sub)
     end
-    Rails.logger.debug "Costs per team per month data: #{@cost_data.inspect}"
-  end
 
-  def tool_cost(tool)
-    plan = tool.plans.first
-    return 0.0 unless plan
-
-    formula = plan.formula.is_a?(String) ? JSON.parse(plan.formula) : plan.formula
-    formula = formula.first if formula.is_a?(Array)
-    key = determine_key(formula)
-    formula[key].to_f
-  rescue JSON::ParserError => e
-    Rails.logger.error "Error parsing formula for plan #{plan.id}: #{e.message}"
-    0.0
-  end
-
-  def determine_key(formula)
-    formula_keys = formula.is_a?(Hash) ? formula.keys : []
-    # Determine the key based on your business logic
-    # For example, if you want to prioritize 'business' over 'enterprise' and 'free'
-    if formula_keys.include?('business')
-      'business'
-    elsif formula_keys.include?('enterprise')
-      'enterprise'
-    else
-      'free'
-    end
+    @cost_data = object
   end
 
   def active_licenses_per_month
